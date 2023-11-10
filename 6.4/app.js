0142bfd9bfd382fa9a5c3194e5c7cc82f34aa208
fondse <damian.jf2209@gmail.com>
2023-11-10 11:33:08 +0100

    6.4

---
+++ 6.4/app.js
@@ -0,0 +1,23 @@
+function calculate(num, num2) {
+    let answer = num + num2;
+    console.log(answer);
+    document.writeln(answer)
+}
+
+calculate(1,12)
+
+function multiplyByFive(num) {
+    let answer = num * 5;
+    console.log(answer);
+    document.writeln(answer);
+}
+
+multiplyByFive(5)
+
+function calculateMinutes(num) {
+    let answer = num / 60 ;
+    console.log(answer);
+    document.writeln(answer);
+}
+
+calculateMinutes(3600)
