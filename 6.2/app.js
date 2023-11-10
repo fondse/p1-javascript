944c56d8894a1f1a0f97d7b26cbe39052dd29a7d
fondse <damian.jf2209@gmail.com>
2023-11-10 10:55:13 +0100

    6.2

---
+++ 6.2/app.js
@@ -0,0 +1,7 @@
+/* Title */
+
+function kleurAanpassen() {
+    document.getElementById("title").style.color="blue";
+    document.getElementById("title").innerHTML = "Andere kleur!"
+    console.log
+}
